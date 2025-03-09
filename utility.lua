2bcff092d477c09988a371d5cc0598b8d8d9213a
EntonioDMI <EntonioDMI@gmail.com>
2025-03-09 11:36:53 +0500

    Update utility.lua

--- utility.lua
+++ utility.lua
@@ -22,75 +22,43 @@ local originalSettings = {
     FogColor = Lighting.FogColor
 }
 
--- World Customization Functions
-function UtilityModule:ToggleCustomTime(state)
-    self.Settings.customTimeEnabled = state
-    if state then
-        Lighting.TimeOfDay = self.Settings.timeOfDay
-    else
-        Lighting.TimeOfDay = originalSettings.TimeOfDay
-    end
-end
-
-function UtilityModule:SetTimeOfDay(time)
-    self.Settings.timeOfDay = time
-    if self.Settings.customTimeEnabled then
-        Lighting.TimeOfDay = time
-    end
-end
-
-function UtilityModule:ToggleCustomLighting(state)
-    self.Settings.customLightingEnabled = state
-    if state then
-        Lighting.Ambient = self.Settings.ambientColor
-    else
-        Lighting.Ambient = originalSettings.Ambient
-    end
-end
-
-function UtilityModule:SetAmbientColor(color)
-    self.Settings.ambientColor = color
-    if self.Settings.customLightingEnabled then
-        Lighting.Ambient = color
-    end
-end
-
-function UtilityModule:ToggleCustomFog(state)
-    self.Settings.customFogEnabled = state
-    if state then
-        Lighting.FogStart = self.Settings.fogStart
-        Lighting.FogEnd = self.Settings.fogEnd
-        Lighting.FogColor = self.Settings.fogColor
-    else
-        Lighting.FogStart = originalSettings.FogStart
-        Lighting.FogEnd = originalSettings.FogEnd
-        Lighting.FogColor = originalSettings.FogColor
-    end
-end
-
-function UtilityModule:SetFogStart(value)
-    self.Settings.fogStart = value
-    if self.Settings.customFogEnabled then
+-- Settings observer
+local function updateSettings(setting, value)
+    UtilityModule.Settings[setting] = value
+    
+    if setting == "customTimeEnabled" then
+        Lighting.TimeOfDay = value and UtilityModule.Settings.timeOfDay or originalSettings.TimeOfDay
+    elseif setting == "timeOfDay" and UtilityModule.Settings.customTimeEnabled then
+        Lighting.TimeOfDay = value
+    elseif setting == "customLightingEnabled" then
+        Lighting.Ambient = value and UtilityModule.Settings.ambientColor or originalSettings.Ambient
+    elseif setting == "ambientColor" and UtilityModule.Settings.customLightingEnabled then
+        Lighting.Ambient = value
+    elseif setting == "customFogEnabled" then
+        if value then
+            Lighting.FogStart = UtilityModule.Settings.fogStart
+            Lighting.FogEnd = UtilityModule.Settings.fogEnd
+            Lighting.FogColor = UtilityModule.Settings.fogColor
+        else
+            Lighting.FogStart = originalSettings.FogStart
+            Lighting.FogEnd = originalSettings.FogEnd
+            Lighting.FogColor = originalSettings.FogColor
+        end
+    elseif setting == "fogStart" and UtilityModule.Settings.customFogEnabled then
         Lighting.FogStart = value
-    end
-end
-
-function UtilityModule:SetFogEnd(value)
-    self.Settings.fogEnd = value
-    if self.Settings.customFogEnabled then
+    elseif setting == "fogEnd" and UtilityModule.Settings.customFogEnabled then
         Lighting.FogEnd = value
+    elseif setting == "fogColor" and UtilityModule.Settings.customFogEnabled then
+        Lighting.FogColor = value
     end
 end
 
-function UtilityModule:SetFogColor(color)
-    self.Settings.fogColor = color
-    if self.Settings.customFogEnabled then
-        Lighting.FogColor = color
-    end
+-- Public method for updating settings
+function UtilityModule:UpdateSetting(setting, value)
+    updateSettings(setting, value)
 end
 
 function UtilityModule:Initialize()
-    -- Store original settings when module is initialized
     originalSettings.TimeOfDay = Lighting.TimeOfDay
     originalSettings.Ambient = Lighting.Ambient
     originalSettings.FogStart = Lighting.FogStart
