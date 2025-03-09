28abc9fa57c43634267c1cbefbaae9c3434e27df
EntonioDMI <EntonioDMI@gmail.com>
2025-03-09 11:21:48 +0500

    Create utility.lua

---
+++ utility.lua
@@ -0,0 +1,13 @@
+local UtilityModule = {}
+
+-- Module Configuration
+UtilityModule.Settings = {
+    -- Will be populated with utility features
+}
+
+-- Utility Functions
+function UtilityModule:Initialize()
+    -- Initialization logic will be implemented here
+end
+
+return UtilityModule
